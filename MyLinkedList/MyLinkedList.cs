d6e12023df886777cd03fc89959c145f83bdd84f
Corrado <corrado.torino@gmail.com>
2020-11-27 23:37:36 +0100

    draft MyLinkedList

---
+++ MyLinkedList/MyLinkedList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BrainTrain.MyLinkedList
+{
+    public class MyLinkedList<T>
+    {
+        private MyLinkedListNode<T> first = null;
+
+        public void AddFirst(MyLinkedListNode<T> node)
+        {
+            this.first = node;
+        }
+
+        public MyLinkedListNode<T> First => first;
+
+        public MyLinkedListNode<T> Last {
+            get { return first; }
+        }
+
+        public void AddAfter(MyLinkedListNode<T> node, MyLinkedListNode<T> newNode)
+        {
+            if ( node == null)
+            {
+                throw new ArgumentNullException(nameof(node));
+            }
+
+            if (newNode == null)
+            {
+                throw new ArgumentNullException(nameof(newNode));
+            }
+
+        }
+    }
+}
---
+++ MyLinkedList/MyLinkedListTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrainTrain.MyLinkedList
+{
+    [TestClass]
+    public class MyLinkedListTests
+    {
+        [TestMethod]
+        public void WhenAddFirst_FirstIsReturned()
+        {
+            // Arrange
+            var node = new MyLinkedListNode<int>();
+            var sut = new MyLinkedList<int>();
+
+            // Act
+            sut.AddFirst(node);
+
+            // Assert
+            sut.First.Should().Be(node);
+        }
+
+        [TestMethod]
+        public void WhenAddFirst_LastIsReturned()
+        {
+            // Arrange
+            var node = new MyLinkedListNode<int>();
+            var sut = new MyLinkedList<int>();
+
+            // Act
+            sut.AddFirst(node);
+
+            // Assert
+            sut.Last.Should().Be(node);
+        }
+
+        [TestMethod]
+        public void AddAfter_WhenNodeIsNull_ThrowArgumentNullException()
+        {
+            // Arrange
+            var sut = new MyLinkedList<int>();
+
+            // Act
+            var action = new Action(()=> sut.AddAfter(null, new MyLinkedListNode<int>() ));
+
+            // Assert
+            action.Should().Throw<ArgumentNullException>().WithMessage("Value cannot be null. (Parameter 'node')");
+        }
+
+        [TestMethod]
+        public void AddAfter_WhenNewNodeIsNull_ThrowArgumentNullException()
+        {
+            // Arrange
+            var sut = new MyLinkedList<int>();
+
+            // Act
+            var action = new Action(() => sut.AddAfter( new MyLinkedListNode<int>(), null));
+
+            // Assert
+            action.Should().Throw<ArgumentNullException>().WithMessage("Value cannot be null. (Parameter 'newNode')");
+        }
+
+        [TestMethod]
+        public void AddAfter_()
+        {
+            // Arrange
+            var firstNode = new MyLinkedListNode<int>();
+            var secondNode = new MyLinkedListNode<int>();
+            var sut = new MyLinkedList<int>();
+
+            sut.AddFirst(firstNode);
+
+            // Act
+            sut.AddAfter(firstNode, secondNode);
+
+            // Assert
+            sut.First.Should().Be(firstNode);
+        }
+
+        [TestMethod]
+        public void test()
+        {
+            var a = new LinkedList<int>();
+
+            
+        }
+    }
+
+    public class MyLinkedListNode<T>
+    {
+    }
+}
